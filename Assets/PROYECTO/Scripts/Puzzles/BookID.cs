19b6caeb9a08a21bdd6e19a819560fed930d354c
Refex38 <182239@upslp.edu.mx>
2026-05-09 15:59:24 -0600

    Arreglada mano derecha del jugador y agregado nuevo minjuego de libros

---
+++ Assets/PROYECTO/Scripts/Puzzles/BookID.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BookID : MonoBehaviour
+{
+    [Tooltip("Asigna un número único para cada libro")]
+    public int id;
+}
